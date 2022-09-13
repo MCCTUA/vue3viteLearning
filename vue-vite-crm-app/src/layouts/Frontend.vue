db6162834807c49e68b8f19a06cd0185a0c7c676
MCCTUA <mcctua@gmail.com>
2022-09-13 22:23:18 +0700

    front end layout

---
+++ vue-vite-crm-app/src/layouts/Frontend.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Nav />
+    <main>
+      <router-view />
+    </main>
+    <Foot />
+  </div>
+</template>
+<script>
+import Nav from '@/components/fronted/Nav.vue'
+import Foot from '@/components/fronted/Foot.vue'
+export default {
+  name: 'FrontendLayout',
+  components: {
+    Nav,
+    Foot
+  }
+}
+</script>
+<style lang=""></style>
--- vue-vite-crm-app/src/router/index.js
+++ vue-vite-crm-app/src/router/index.js
@@ -1,19 +1,95 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+
+// Import Frontend Layout
+
+import FrontendLayout from '@/layouts/Frontend.vue'
+
+// import Frontend Page
+import Home from '@/views/frontend/Home.vue'
+import About from '@/views/frontend/About.vue'
+import Portfolio from '@/views/frontend/Portfolio.vue'
+import Service from '@/views/frontend/Service.vue'
+import Contact from '@/views/frontend/Contact.vue'
+import Register from '@/views/frontend/Register.vue'
+import Login from '@/views/frontend/Login.vue'
+import ForgotPassword from '@/views/frontend/ForgetPassword.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // Fronend Route
     {
+      // เรียก Fronted Layout
       path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView
+      name: 'Frontend',
+      component: FrontendLayout,
+      // เรียก Component ตัวลูกมาใส่
+      children: [
+        {
+          path: '',
+          name: 'Home',
+          component: Home,
+          meta: {
+            title: 'หน้าหลัก'
+          }
+        },
+        {
+          path: 'about',
+          name: 'About',
+          component: About,
+          meta: {
+            title: 'เกี่ยวกับเรา'
+          }
+        },
+        {
+          path: 'portfolio',
+          name: 'Portfolio',
+          component: Portfolio,
+          meta: {
+            title: 'ผลงานของเรา'
+          }
+        },
+        {
+          path: 'service',
+          name: 'Service',
+          component: Service,
+          meta: {
+            title: 'บริการของเรา'
+          }
+        },
+        {
+          path: 'contact',
+          name: 'Contact',
+          component: Contact,
+          meta: {
+            title: 'ติดต่อเรา'
+          }
+        },
+        {
+          path: 'register',
+          name: 'Register',
+          component: Register,
+          meta: {
+            title: 'สมัครสมาชิก'
+          }
+        },
+        {
+          path: 'login',
+          name: 'Login',
+          component: Login,
+          meta: {
+            title: 'เข้าสู่ระบบ'
+          }
+        },
+        {
+          path: 'forgotpassword',
+          name: 'ForgotPassword',
+          component: ForgotPassword,
+          meta: {
+            title: 'ลืมรหัสผ่าน'
+          }
+        }
+      ]
     }
   ]
 })
